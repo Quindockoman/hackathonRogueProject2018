9d330772e38a7e1f97e37c0e72e4562b18ae2554
will <commanderwill2142@gmail.com>
2018-11-16 18:39:05 -0700

    Created Item Class

---
+++ Item.py
@@ -0,0 +1,13 @@
+# Item Class
+
+class item:
+    def __init__(self, status, type, bonus, name, description, event, omen):
+        self.status = status
+        self.type = type
+        self.bonus = bonus
+        self.name = name
+        self.description = description
+        self.event = event
+        self.omen = omen
+
+
